81d5f31485bb654c628eb4128f776720ea16e044
home <nuttapong_coach@hotmail.com>
2025-04-15 14:43:58 +0700

    add code

--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FrontEnd;
 using FrontEnd.Services;
 using NetcodeHub.Packages.Extensions.LocalStorage;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -13,5 +15,6 @@ builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddNetcodeHubLocalStorageService();
 builder.Services.AddScoped<BroadcastService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthState>();
 
 await builder.Build().RunAsync();
